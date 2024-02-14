fc1e0a4d3139ad960bee68c6e282d604fe65eb77
Emilien Cosson <emilien@em-ilien.fr>
2024-02-14 11:19:56 +0100

    add test classes

---
+++ app/src/test/java/CompteTest.java
@@ -0,0 +1,107 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CompteTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCompteIntPersonne() {
+		// TODO
+	}
+
+	@Test
+	void testCompteIntPersonneDouble() {
+		// TODO
+	}
+
+	@Test
+	void testCompteIntPersonneDoubleDoubleDouble() {
+		// TODO
+	}
+
+	@Test
+	void testChangerDecouvertMaximalAutorise() {
+		// TODO
+	}
+
+	@Test
+	void testChangerDebitMaximalAutorise() {
+		// TODO
+	}
+
+	@Test
+	void testCrediterSolde() {
+		// TODO
+	}
+
+	@Test
+	void testDebiterSolde() {
+		// TODO
+	}
+
+	@Test
+	void testExecuterVirement() {
+		// TODO
+	}
+
+	@Test
+	void testEstADecouvert() {
+		// TODO
+	}
+
+	@Test
+	void testDecouvert() {
+		// TODO
+	}
+
+	@Test
+	void testTitulaire() {
+		// TODO
+	}
+
+	@Test
+	void testNumero() {
+		// TODO
+	}
+
+	@Test
+	void testSolde() {
+		// TODO
+	}
+
+	@Test
+	void testDecouvertMaximalAutorise() {
+		// TODO
+	}
+
+	@Test
+	void testDebitMaximalAutorise() {
+		// TODO
+	}
+
+	@Test
+	void testMontantMaximumDebitAutorise() {
+		// TODO
+	}
+
+}
---
+++ app/src/test/java/fr/em_ilien/bankaccount/PersonneTest.java
@@ -0,0 +1,54 @@
+package fr.em_ilien.bankaccount;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonneTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testPersonne() {
+		// TODO
+	}
+
+	@Test
+	void testNom() {
+		// TODO
+	}
+
+	@Test
+	void testPrenom() {
+		// TODO
+	}
+
+	@Test
+	void testAdresse() {
+		// TODO
+	}
+
+	@Test
+	void testChangerAdresse() {
+		// TODO
+	}
+
+}
